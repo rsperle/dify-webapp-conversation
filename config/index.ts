763b37fb4d97ff4312a62c799c5d952915ff0a66
Raphael Sperle <rsperle@gmail.com>
2024-05-21 22:15:53 -0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -7,7 +7,7 @@ export const APP_INFO: AppInfo = {
   description: 'Assistente de pré-venda da Prolotes, a maior loteadora do Estado do Rio de Janeiro.',
   copyright: '',
   privacy_policy: '',
-  default_language: 'pt-br',
+  default_language: 'en',
 }
 
 export const isShowPrompt = false
