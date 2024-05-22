d5057a5d54e1d166a8510b8037ef8fc2e819d4f0
Raphael Sperle <rsperle@gmail.com>
2024-05-21 22:06:29 -0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'Prolotes',
+  description: 'Assistente de pré-venda da Prolotes, a maior loteadora do Estado do Rio de Janeiro.',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'pt-BR',
 }
 
 export const isShowPrompt = false
